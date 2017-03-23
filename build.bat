4a4b1d8e3a5db27ba733ec6a0c38e701707f78c7
ms <ms@camcad.de>
2017-03-23 17:13:09 +0100

    test

--- build.bat
+++ build.bat
@@ -1,4 +1,12 @@
+@SET BDS=d:\CodeGear\RAD Studio\5.0
+@SET BDSCOMMONDIR=C:\Users\Public\Documents\RAD Studio\5.0
+@SET FrameworkDir=C:\Windows\Microsoft.NET\Framework\
+@SET FrameworkVersion=v2.0.50727
+@SET FrameworkSDKDir=
 
-"d:\CodeGear\RAD Studio\5.0\bin\rsvars.bat"
+@ECHO Setting environment for using CodeGear RAD Studio tools 2
+
+@SET PATH=%FrameworkDir%%FrameworkVersion%;%FrameworkSDKDir%;%PATH%
+
 
 "c:\Windows\Microsoft.NET\Framework\v2.0.50727\MSBuild.exe" SDIAPP.dproj > CON
