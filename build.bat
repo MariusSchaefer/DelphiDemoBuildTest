5764a3f2a2d033c9efdb40821cb93b345bb37cce
ms <ms@camcad.de>
2017-03-23 17:16:02 +0100

    test

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 
 call "d:\CodeGear\RAD Studio\5.0\bin\rsvars.bat"
 
-"c:\Windows\Microsoft.NET\Framework\v2.0.50727\MSBuild.exe" SDIAPP.dproj > CON
+"c:\Windows\Microsoft.NET\Framework\v2.0.50727\MSBuild.exe" SDIAPP.dproj  /verbosity:n 
